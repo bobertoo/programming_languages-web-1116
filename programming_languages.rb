cdbd95ba66cf101eba5cb6ddd9671b2d83cedb42
Vincente Martinez <vinny@vinnymartinez.com>
2016-11-07 01:04:32 +0000

    Done.

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,3 +1,16 @@
 def reformat_languages(languages)
   # your code here
+  reform = {}
+  arr = []
+  languages.each do |keyz, valz|
+    valz.each do |language, lan_type|
+      if reform[language]
+        reform[language][:style] << keyz
+      else
+        arr = [keyz]
+        reform[language] = {:type => lan_type.values[0], :style => arr}
+      end
+    end
+  end
+  reform
 end
